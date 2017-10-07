26635d895d128d31dbdf83b59d026ecfd00d7356
Gonzalo Saad <saad.gonzalo.ale@gmail.com>
2017-10-07 10:06:24 -0300

    Modificando RandomCreator

--- src/cu68_dsi_entrega4/CU68_DSI_ENTREGA4.java
+++ src/cu68_dsi_entrega4/CU68_DSI_ENTREGA4.java
@@ -4,11 +4,14 @@
  * and open the template in the editor.
  */
 package cu68_dsi_entrega4;
+
 import clases_analisis.GestorControlLectura;
 import clases_dominio.Lectura;
 import java.util.Date;
 import java.util.Calendar;
 import randomCreator.RandomCreator;
+import java.util.Random;
+
 /**
  *
  * @author Gonzalo
@@ -20,11 +23,20 @@ public class CU68_DSI_ENTREGA4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         GestorControlLectura gestor = new GestorControlLectura();
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.MONTH, -5);
+        
+        Calendar cal2 = Calendar.getInstance();
+        
         RandomCreator rc = new RandomCreator();
         
-        
+        int cant = 4;
+        int[] v = rc.distributeSumInCant(50, cant);
+        for(int c: v){
+            System.out.println(c);
+        }
     }
-    
+
 }
--- src/randomCreator/RandomCreator.java
+++ src/randomCreator/RandomCreator.java
@@ -17,45 +17,50 @@ import java.util.Calendar;
  */
 public class RandomCreator {
 
-    public Lectura[] createLecturas(Date dateFrom, int cant,double initialRead) {
+    private Instalacion[] createInstalaciones(Date dateFrom, int cant, double initialRead) {
+        Instalacion inst[] = new Instalacion[cant];
+        int months = monthDiffToDate(dateFrom);
+
+        return null;
+    }
+
+    public Lectura[] createLecturas(Date dateFrom, int cant, double initialRead) {
         Lectura lec[] = new Lectura[cant];
-        double reads[] = randomDoubleVector(initialRead,cant);
+        double reads[] = randomDoubleVector(initialRead, cant);
         Calendar cal = Calendar.getInstance();
         cal.setTime(dateFrom);
-        
-        for (int i = 0; i < cant;i++){
+
+        for (int i = 0; i < cant; i++) {
             Lectura nLec = new Lectura();
             nLec.setFechaHoraLectura(cal.getTime());
             nLec.setValorLectura(reads[i]);
-            if (i<cant-1){
+            if (i < cant - 1) {
                 nLec.setEstado(new ControladaFacturada());
-            }
-            else{
+            } else {
                 nLec.setEstado(randomState());
             }
-            
-            lec[i]=nLec;
-            cal.add(Calendar.MONTH, 1);
+
+            lec[i] = nLec;
+            cal.add(Calendar.DATE, 1);
         }
-            
-        
+
         return lec;
 
     }
 
     private int randomInt(int minimum, int maximum, int multiplier) {
         Random random = new Random();
-        return minimum + random.nextInt(maximum) * multiplier;
+        return minimum + random.nextInt(maximum-minimum) * multiplier;
     }
-    
-    private Estado randomState(){
-        Estado est = null; 
-        int num = randomInt(1,4,1);
-        switch (num){
+
+    private Estado randomState() {
+        Estado est = null;
+        int num = randomInt(1, 4, 1);
+        switch (num) {
             case 1:
                 est = new Creada();
                 break;
-            case 2: 
+            case 2:
                 est = new ControladaSinFacturar();
                 break;
             case 3:
@@ -64,18 +69,53 @@ public class RandomCreator {
             case 4:
                 est = new ControladaFacturada();
                 break;
-            
+
         }
         return est;
     }
-    
-    private double[] randomDoubleVector(double initialValue, int cant){
+
+    private double[] randomDoubleVector(double initialValue, int cant) {
         double vec[] = new double[cant];
         double value = initialValue;
+
+        for (int i = 0; i < cant; i++) {
+            vec[i] = value;
+            value += 25 * randomInt(0, 20, 1);
+        }
+        return vec;
+    }
+
+    private int monthDiff(Date endDate, Date startDate) {
+        Calendar startCalendar = Calendar.getInstance();
+        startCalendar.setTime(startDate);
+        Calendar endCalendar = Calendar.getInstance();
+        endCalendar.setTime(endDate);
+
+        int diffYear = endCalendar.get(Calendar.YEAR) - startCalendar.get(Calendar.YEAR);
+        int diffMonth = diffYear * 12 + endCalendar.get(Calendar.MONTH) - startCalendar.get(Calendar.MONTH);
+        return diffMonth;
+    }
+
+    private int monthDiffToDate(Date startDate) {
+        return monthDiff(Calendar.getInstance().getTime(), startDate);
+    }
+
+    public int[] distributeSumInCant(int sum, int cant) {
+        int[] vec = new int[cant];
         
-        for (int i = 0; i < cant;i++){
-            vec[i]=value;
-            value += 25*randomInt(0,20,1);
+        int tot = sum;
+        int top;
+        int num;
+        for (int i = 0; i < cant; i++) {
+            if (i < cant - 1) {
+                top = (int)(tot * randomInt(0,5,1)/10);
+                num = randomInt(3,top,1);   
+                tot = tot - num;
+            }
+            else{
+                num = sum - tot;
+            }
+            vec[i]=num;
         }
         return vec;
     }
