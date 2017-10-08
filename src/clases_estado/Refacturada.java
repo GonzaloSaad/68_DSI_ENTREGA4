4ba66627d49338e5980fc727328022f7d301e3b5
Gonzalo Saad <saad.gonzalo.ale@gmail.com>
2017-10-08 12:54:51 -0300

    las clases que faltabas que heredan de Estado

---
+++ src/clases_estado/Refacturada.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases_estado;
+
+/**
+ *
+ * @author estre
+ */
+public class Refacturada extends Estado {
+    @Override
+    public boolean esRefacturada(){
+        return true;
+    }
+    
+}
